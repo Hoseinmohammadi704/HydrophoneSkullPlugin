1d7946b2a1c7313442e9e8219b7600e29c62157a
jws2f <jsnell@fusfoundation.org>
2017-05-03 09:54:57 -0400

    Saving CPC ACT file for hydrophone testing Fixed some flashing issues and pressure map orientation issue SDR analysis distribution prep Saving skull parameters to a file Incident angle and SDR indicator layout redone CT images with multiple C/W attributes handled Readying for another hydrophone test Fixed tx envelope bug Plugin architecture Fixed some steering transform issues and thermometry plane alignment Better thermometry shader Logo Selection of different time phases for thermometry display

---
+++ src/org/fusfoundation/kranion/plugins/HydrophoneSkullPlugin.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fusfoundation.kranion.plugins;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.fusfoundation.kranion.Button;
+import org.fusfoundation.kranion.ProgressListener;
+import org.fusfoundation.kranion.Renderable;
+import org.fusfoundation.kranion.FlyoutPanel;
+import org.fusfoundation.kranion.plugin.Plugin;
+
+
+import org.fusfoundation.kranion.view.View;
+import org.fusfoundation.kranion.model.*;
+import org.fusfoundation.kranion.controller.Controller;
+import org.fusfoundation.kranion.model.image.ImageVolume;
+import org.fusfoundation.kranion.model.image.ImageVolume4D;
+
+import org.lwjgl.util.vector.Vector3f;
+
+/**
+ *
+ * @author john
+ */
+public class HydrophoneSkullPlugin implements Plugin, ActionListener {
+    private Model model;
+    private View view;
+    private Controller controller;
+    private FiducialFinder fiducialFinder = new FiducialFinder();
+    
+    @Override
+    public String getName() {
+        return "HydrophoneSkullPlugin";
+    }
+    
+    @Override
+    public void init(Controller controller) {
+        this.model = controller.getModel();
+        this.view = controller.getView();
+        this.controller = controller;
+
+        System.out.println("******* Hello from HydrophoneSkullPlugin !! ****************");
+
+        controller.addActionListener(this);
+
+        Renderable mainPanel = Renderable.lookupByTag("MainFlyout");
+        if (mainPanel != null && mainPanel instanceof FlyoutPanel) {
+            FlyoutPanel panel = (FlyoutPanel) mainPanel;
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 350, 250, 220, 25, controller).setTitle("Find Fiducials").setCommand("findFiducials"));
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1280, 190, 200, 25, controller).setTitle("Show my loc.").setCommand("ShowMyLoc"));
+
+//        // to move the transducer on X axis
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1280, 140, 100, 35, controller).setTitle(" Tx +X").setCommand("TxShiftToPlusDirX"));
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1280, 100, 100, 35, controller).setTitle(" Tx -X").setCommand("TxShiftToMinusDirX"));
+//        // to move the transducer on Y axis
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1410, 140, 100, 35, controller).setTitle(" Tx +Y").setCommand("TxShiftToPlusDirY"));
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1410, 100, 100, 35, controller).setTitle(" Tx -Y").setCommand("TxShiftToMinusDirY"));
+//        // to move the transducer on Z axis
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1540, 140, 100, 35, controller).setTitle(" Tx +Z").setCommand("TxShiftToPlusDirZ"));
+            panel.addChild(new Button(Button.ButtonType.BUTTON, 1540, 100, 100, 35, controller).setTitle(" Tx -Z").setCommand("TxShiftToMinusDirZ"));
+        }
+    }
+    
+    @Override
+    public void release() {
+        controller = null;
+        model = null;
+        view = null;
+        
+        controller.removeActionListener(this);        
+    }    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("findFiducials")) {
+            ImageVolume4D image = (ImageVolume4D)model.getCtImage();
+            if (image != null && model != null && view != null) {
+                if (fiducialFinder.find(image, controller)) {               
+                     model.setCtImage(image);
+                     model.setAttribute("currentTargetPoint", new Vector3f());
+                     view.setIsDirty(true);
+                }
+            }
+        }                
+        else if (e.getActionCommand().equals("ShowMyLoc")) {
+            try {
+
+                System.out.println(" ");
+                System.out.println("** My Location is: ");
+
+                try {
+                    Vector3f transCT = (Vector3f) model.getCtImage().getAttribute("ImageTranslation");
+                    System.out.println("** CT: X axis: " + transCT.x + ",Y axis: " + transCT.y + ", Z axis: " + transCT.z);
+                } catch (Exception ect) {
+                    System.out.println("\t No CT found.");
+                    //ect.printStackTrace();
+                }
+
+                try {
+                    Vector3f transMR = (Vector3f) model.getMrImage(0).getAttribute("ImageTranslation");
+                    System.out.println("** MR: X axis: " + transMR.x + ",Y axis: " + transMR.y + ", Z axis: " + transMR.z);
+                } catch (Exception emr) {
+                    System.out.println("\t No MR found.");
+                    //emr.printStackTrace();
+                }
+                System.out.println(" ");
+
+            } catch (Exception ei) {
+                ei.printStackTrace();
+            }
+            
+        }
+        else if (e.getActionCommand().equals("TxShiftToPlusDirX")) {
+  
+            shiftMRCT( 1, 10); // shift 1 = 1mm  
+        }
+                
+        else if (e.getActionCommand().equals("TxShiftToMinusDirX")) {             
+              
+            shiftMRCT( 1, -10);
+        }
+        else if (e.getActionCommand().equals("TxShiftToPlusDirY")) {
+  
+            shiftMRCT( 2, 10);   
+        }
+                
+        else if (e.getActionCommand().equals("TxShiftToMinusDirY")) {             
+              
+            shiftMRCT( 2, -10);
+        }
+        else if (e.getActionCommand().equals("TxShiftToPlusDirZ")) {
+  
+            shiftMRCT( 3, 10);   
+        }
+                
+        else if (e.getActionCommand().equals("TxShiftToMinusDirZ")) {             
+              
+            shiftMRCT( 3, -10);
+        }
+    }
+    
+    private void shiftMRCT(int AxisNumb, float shiftValue) {
+        try{
+            ImageVolume ctimage = model.getCtImage();
+            ImageVolume mrimage = model.getMrImage(0);
+            
+            Vector3f transCT = new Vector3f();
+            if (ctimage != null) {
+                transCT = (Vector3f)ctimage.getAttribute("ImageTranslation");
+            }
+            Vector3f transMR = new Vector3f();
+            if (mrimage != null) {
+                transMR = (Vector3f)mrimage.getAttribute("ImageTranslation");
+            }
+
+            if (AxisNumb == 1){
+                transCT.x += shiftValue;
+                transMR.x += shiftValue;
+            }else if(AxisNumb ==2){
+                transCT.y += shiftValue;
+                transMR.y += shiftValue;
+            }else if(AxisNumb ==3){
+                transCT.z += shiftValue;
+                transMR.z += shiftValue;
+            }
+
+            if (ctimage != null) {
+                ctimage.setAttribute("ImageTranslation", transCT);
+            }
+           
+            if (mrimage != null) {
+                mrimage.setAttribute("ImageTranslation", transMR);
+            }
+            
+            System.out.println(" "); 
+            System.out.println("** CT: X axis: " + transCT.x + ",Y axis: " + transCT.y + ", Z axis: " +transCT.z);
+            System.out.println("** MR: X axis: " + transMR.x + ",Y axis: " + transMR.y + ", Z axis: " +transMR.z);
+            System.out.println(" ");
+            
+            view.setIsDirty(true);
+            
+        }
+        catch(Exception ei) {
+            ei.printStackTrace();
+        }
+    }
+}
